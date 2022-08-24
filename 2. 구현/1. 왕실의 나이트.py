ec1bf5260887e5d744c1ac360d5a1700820da90b
Monad00 <ideaoclock@gmail.com>
2022-08-24 22:39:28 +0900

    add

--- 2. 구현/1. 왕실의 나이트.py
+++ 2. 구현/1. 왕실의 나이트.py
@@ -16,7 +16,7 @@ print(count)
 input_data = input()
 row = int(input_data[1])
 column = int(ord(input_data[0])) - int(ord('a')) + 1
-
+                    
 steps = [(-2, 1), (-2, -1), (2, 1), (2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
 
 result = 0
@@ -25,6 +25,6 @@ for step in steps:
     next_column = column + step[1]
 
     if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
-        result += 1
+        result += 16
 
 print(result)
