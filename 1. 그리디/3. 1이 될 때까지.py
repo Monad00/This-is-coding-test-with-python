ed48876b7cb93762d15e06c34a208b1a7024a46f
Yul Choi <110292533+Monad00@users.noreply.github.com>
2022-10-03 18:12:22 +0900

    Add files via upload

---
+++ 1. 그리디/3. 1이 될 때까지.py
@@ -0,0 +1,26 @@
+n,k = map(int, input().split())
+
+cnt = 0
+while n != 1:
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+    cnt += 1
+print(cnt)
+
+# 답안 예시
+n,k = map(int, input().split())
+
+result = 0
+while True:
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    if n < k:
+        break
+    result += 1
+    n //= k
+
+result += (n - 1)
+print(result)
